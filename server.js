51afa13a868dea4203c2925bc14d3b8345afc8f0
Anny Gutierrez <32302890+Gloper98@users.noreply.github.com>
2018-02-15 02:41:26 -0500

    server.js
    updating changes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const server = app.listen(3000, encender);
+const server = app.listen(3002, encender);
 
 function encender(){
 console.log('servidor encendido');
